4690ea7d94fbee562b8b8322dd60a6d799b405cb
Matthew Cheng <mbcheng2@.illinois.edu>
2019-04-19 00:06:26 -0500

    remove unused files and removed them from project file list

--- sat_counter.sv
+++
@@ -1,9 +0,0 @@
-module sat_counter (
-    input logic clk, 
-    input logic [1:0] counter_val, 
-    input logic do_count,       
-    input logic is_count_up, 
-    output logic [1:0] new_counter_val
-);
-
-endmodule: sat_counter 
